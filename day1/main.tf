fb6b54386a98d15349d75eaee23b88210b1863f7
This-is-Mani <207173292+This-is-Mani@users.noreply.github.com>
2026-06-24 09:22:02 +0100

    Update main.tf

--- day1/main.tf
+++ day1/main.tf
@@ -1,3 +1,4 @@
+#initiatlizing the terraform
 terraform {
   required_providers {
     aws = {
@@ -7,14 +8,26 @@ terraform {
   }
 }
 
+#setting up provider
 provider "aws" {
   region = "us-east-1"
 }
 
+#creating s3 bucket resource on aws
 resource "aws_s3_bucket" "demo" {
   bucket = "my-terraform-demo-bucket-12345"
   tags = {
     Environment = "dev"
     Owner       = "you"
+  }
+}
+
+#creating an ec2 instance
+resource "aws_instance" "demo" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (example)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-demo-instance"
   }
 }
