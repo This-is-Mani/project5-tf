cbf1e7441be083168381be400f97678cc343859b
This-is-Mani <207173292+This-is-Mani@users.noreply.github.com>
2026-06-24 09:20:14 +0100

    Update main.tf

--- day1/main.tf
+++ day1/main.tf
@@ -10,3 +10,11 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "my-terraform-demo-bucket-12345"
+  tags = {
+    Environment = "dev"
+    Owner       = "you"
+  }
+}
