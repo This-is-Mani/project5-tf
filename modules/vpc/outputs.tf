dd39921481b93420ddcde989476674ec25bcd641
This-is-Mani <207173292+This-is-Mani@users.noreply.github.com>
2026-06-24 12:47:36 +0100

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
+
+output "ssh_sg_id" {
+  value = aws_security_group.ssh.id
+}
